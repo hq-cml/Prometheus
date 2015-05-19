8f7f61ac21d5d9c5c6eabe94d3ae9e47f40be9f4
hq <hq_cml@163.com>
2015-05-20 07:54:45 +0800

    change name

--- Prometheus/CompileClass.php
+++ Prometheus/CompileClass.php
@@ -11,7 +11,7 @@
     {
         private $template;               //待编译的文件
         private $content;                //需要替换的文本
-        private $compiled_file;          //编译后的文件
+        private $compile_file;           //编译后的文件
         private $left = '{';             //左定界符
         private $right = '}';            //有定界符
         private $value  = array();       //值栈
@@ -19,10 +19,10 @@
         private $arr_pattern = array();  //模式数组
         private $arr_replace = array();  //替换数组
         
-        public function __construct($template, $compiled_file, $config)
+        public function __construct($template, $compile_file, $config)
         {
             $this->template      = $template;
-            $this->compiled_file = $compiled_file;
+            $this->compile_file  = $compile_file;
             $this->content       = file_get_contents($template);
             
             if($config['php_turn'] === false)
@@ -45,7 +45,7 @@
             //批量解析
             $this->content = preg_replace($this->arr_pattern, $this->arr_replace, $this->content);
             
-            file_put_contents($this->compiled_file, $this->content);
+            file_put_contents($this->compile_file, $this->content);
         }
         
         
